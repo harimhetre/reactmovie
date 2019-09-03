87560b5c4c3ce28c1b780a8af19b8871aea7b222
Hari Mhetre <hari.mhetre2222@gmail.com>
2019-09-03 23:23:56 +0530

    Updates

---
+++ precache-manifest.bea34baf0de303ecde3c676d42e8737e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd74171fcffd5485b518",
+    "url": "/reactmovie/static/js/runtime~main.fd74171f.js"
+  },
+  {
+    "revision": "0c8cab67be3107aa7fdf",
+    "url": "/reactmovie/static/js/main.0c8cab67.chunk.js"
+  },
+  {
+    "revision": "b9afa4965cedc2c9f759",
+    "url": "/reactmovie/static/js/1.b9afa496.chunk.js"
+  },
+  {
+    "revision": "0c8cab67be3107aa7fdf",
+    "url": "/reactmovie/static/css/main.3c5d59ac.chunk.css"
+  },
+  {
+    "revision": "b9afa4965cedc2c9f759",
+    "url": "/reactmovie/static/css/1.2eb3a1cd.chunk.css"
+  },
+  {
+    "revision": "5aa837d48b73de0dca0fe57f3bd06c35",
+    "url": "/reactmovie/index.html"
+  }
+];
