de493605ed625aa25a29aee9ada32bd5cbd43d0e
Hari Mhetre <hari.mhetre2222@gmail.com>
2019-09-03 23:55:15 +0530

    Updates

--- precache-manifest.e1f732844731efac7851a03907890c84.js
+++ precache-manifest.e1f732844731efac7851a03907890c84.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/reactmovie/static/js/runtime~main.fd74171f.js"
   },
   {
-    "revision": "0c8cab67be3107aa7fdf",
-    "url": "/reactmovie/static/js/main.0c8cab67.chunk.js"
+    "revision": "8ffde2aa8ea12da0411f",
+    "url": "/reactmovie/static/js/main.8ffde2aa.chunk.js"
   },
   {
     "revision": "b9afa4965cedc2c9f759",
     "url": "/reactmovie/static/js/1.b9afa496.chunk.js"
   },
   {
-    "revision": "0c8cab67be3107aa7fdf",
+    "revision": "8ffde2aa8ea12da0411f",
     "url": "/reactmovie/static/css/main.3c5d59ac.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/reactmovie/static/css/1.2eb3a1cd.chunk.css"
   },
   {
-    "revision": "5aa837d48b73de0dca0fe57f3bd06c35",
+    "revision": "bdc523809a354ab72e3a4548d72c2f34",
     "url": "/reactmovie/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactmovie/precache-manifest.bea34baf0de303ecde3c676d42e8737e.js"
+  "/reactmovie/precache-manifest.e1f732844731efac7851a03907890c84.js"
 );
 
 workbox.clientsClaim();
